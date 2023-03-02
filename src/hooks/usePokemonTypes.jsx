3bfed7b8190fb646a062be5eb96ec0f4ff1b135a
nkwaaaa <nkwaaaa@gmail.com>
2023-03-02 12:23:38 -0300

    add sleep helper to test isLoading

--- src/hooks/usePokemonTypes.jsx
+++ src/hooks/usePokemonTypes.jsx
@@ -1,9 +1,10 @@
 import { useQuery } from "react-query";
 import { pokemonApi } from "../api/pokemonApi";
+import { sleep } from "../helpers/sleep";
 
 async function getTypes() {
 	const { data } = await pokemonApi.get("/type");
-
+	await sleep(1.5);
 	return data;
 }
 
