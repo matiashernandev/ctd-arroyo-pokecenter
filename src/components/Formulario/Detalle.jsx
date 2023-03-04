c5fb0a862fc2e7b22ad9f9c25d794dd6b892adc7
nkwaaaa <nkwaaaa@gmail.com>
2023-03-04 12:22:14 -0300

    update mutation

--- src/components/Formulario/Detalle.jsx
+++ src/components/Formulario/Detalle.jsx
@@ -2,39 +2,51 @@
  * Muestra los detalles del formulario y permite enviar la solicitud.
  * @returns {JSX.Element} Componente de detalles del formulario
  */
-
+import { useEffect } from "react";
 import axios from "axios";
 import { useMutation } from "react-query";
 import { useFormularioContext } from "../../context/ContextoFormulario";
 
 const Detalle = () => {
 	//? Con Fetch
-	/* 	const mutation = useMutation(async (formulario) =>
-		fetch("http://localhost:3001/data", {
-			method: "POST",
-			headers: {
-				"Content-type": "application/json; charset=UTF-8",
-			},
-			body: JSON.stringify(formulario),
-		}).then((response) => response.json())
+	const { mutate, isLoading, isError, isSuccess } = useMutation(
+		async (formulario) =>
+			fetch("http://localhost:3001/data", {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+					"Access-Control-Allow-Origin": "*",
+				},
+				body: JSON.stringify(formulario),
+			}).then((response) => response.json())
+	);
+
+	/* 	const { mutate, isLoading, isError, isSuccess } = useMutation(
+		async (formulario) => {
+			try {
+				const response = await axios.post(
+					"http://localhost:3001/data",
+					formulario,
+					{
+						headers: {
+							"Content-type": "application/json; charset=UTF-8",
+						},
+					}
+				);
+				return response.data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		}
 	); */
 
-	const mutation = useMutation(async (formulario) => {
-		try {
-			const response = await axios.post(
-				"http://localhost:3001/data",
-				formulario,
-				{
-					headers: {
-						"Content-type": "application/json; charset=UTF-8",
-					},
-				}
-			);
-			return response.data;
-		} catch (error) {
-			throw new Error(error);
+	useEffect(() => {
+		if (isSuccess) {
+			alert("Formulario enviado y todo bien");
+		} else if (isError) {
+			alert("Error en el formulario");
 		}
-	});
+	}, [isSuccess, isError]);
 
 	//TODO validación de datos, alertas, isError, isLoading, is Fetching, Loader, T_T
 
@@ -56,13 +68,13 @@ const Detalle = () => {
 	 */
 
 	const handleClick = () => {
-		mutation.mutate(formulario);
+		mutate(formulario);
 		//console.log(formulario);
 	};
 
 	return (
 		<div className="detalle-formulario">
-			{mutation.isSuccess ? alert("Se ha creado") : ""}
+			{/* {isSuccess ? alert("Se ha creado") : ""} */}
 
 			<section className="datos-cliente">
 				<h4>Datos del Entrenador</h4>
--- src/hooks/usePokemonTypes.jsx
+++ src/hooks/usePokemonTypes.jsx
@@ -2,11 +2,11 @@ import { useQuery } from "react-query";
 import { pokemonApi } from "../api/pokemonApi";
 import { sleep } from "../helpers/sleep";
 
-async function getTypes() {
+const getTypes = async () => {
 	const { data } = await pokemonApi.get("/type");
 	await sleep(1.5); //? para testear loader
 	return data;
-}
+};
 
 export default function usePokemonTypes() {
 	const typesQuery = useQuery(["types"], getTypes);
